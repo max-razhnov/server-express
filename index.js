9360ca651aef42f44925db78c615f3ea67fb0067
max razhnov <shahteryshka@gmail.com>
2019-09-13 12:41:43 +0300

    feat: add methods request

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 const port = 5000 || process.env.PORT;
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
+const posts = require("./posts");
 
-app.use("/", (request, response) => {
-  console.log(request.url);
-  response.send("Hi from express");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get("/", (request, response) => {
+  const message = "Hi, this is my test server on node js";
+  return response.send(message);
 });
 
-app.use((err, request, response, next) => {
-  console.log(err);
-  response.status(500).send("you broke server");
+app.get("/posts", (req, res) => {
+  return res.send(posts);
 });
 
-app.listen(port);
+app.get("/posts/:id", (req, res) => {
+  const { id } = req.params;
+  return res.send(posts[id]);
+});
+
+app.post("/posts", (req, res) => {
+  const data = req.body;
+  posts.push(data);
+  return res.send(posts);
+});
+
+app.listen(port, () => {
+  console.log(`server start on ${port} port `);
+});
